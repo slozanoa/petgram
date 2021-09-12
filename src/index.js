f393b53fc43cb32ddcfb0fbd9031088de935e0d3
santiago lozano <santiago.lozano@qcode.co>
2021-09-12 18:53:17 -0500

    global styles

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-ReactDOM.render(<h1>Hola que mas!</h1>, document.getElementById('app'))
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./App";
+ReactDOM.render(<App />, document.getElementById("app"));

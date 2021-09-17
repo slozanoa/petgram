56fdafe69e0ae2f650281667b7a305b0728e37e5
santiago lozano <santiago.lozano@qcode.co>
2021-09-16 20:27:34 -0500

    svg

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { ListOfCategories } from "./components/ListOfCategories";
 import { GlobalStyle } from "./GlobalStyles";
+import { ListOfPhotoCard } from "./components/ListOfPhotoCards";
+import { Logo } from "./components/Logo";
 export const App = () => (
   <div>
     <GlobalStyle />
+    <Logo />
     <ListOfCategories />
+    <ListOfPhotoCard />
   </div>
 );

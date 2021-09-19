42ffb04c2388412cead8cb34beafe5531a77a888
santiago lozano <santiago.lozano@qcode.co>
2021-09-18 19:26:09 -0500

    listya de categorias

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import { Category } from "../Category";
 import { List, Item } from "./styles";
-import { categories } from "../../../api/db.json";
 export const ListOfCategories = () => {
-  return (
-    <List>
+  const [categories, setCategories] = useState([]);
+  const [showFixed, setShowFixed] = useState(false);
+
+  useEffect(() => {
+    window
+      .fetch("https://petgram-api-slozanoa.vercel.app/categories")
+      .then((res) => res.json())
+      .then((response) => {
+        setCategories(response);
+      });
+  }, []);
+  useEffect(() => {
+    const onScroll = (e) => {
+      const newShowFixed = window.scrollY > 200;
+      showFixed !== newShowFixed && setShowFixed(newShowFixed);
+    };
+    document.addEventListener("scroll", onScroll);
+
+    return () => document.removeEventListener("scroll", onScroll);
+  }, [showFixed]);
+  const renderList = (fixed) => (
+    <List className={fixed ? "fixed" : ""}>
       {categories.map((category) => (
         <Item key={category.id}>
           <Category {...category} />
         </Item>
       ))}
     </List>
+  );
+  return (
+    <Fragment>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </Fragment>
   );
 };

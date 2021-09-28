3cf4d6a7589846902e7dfb4a1a92c4d014ba4e80
santiago lozano <santiago.lozano@qcode.co>
2021-09-27 20:43:31 -0500

    obtener detalle de la foto

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { ListOfCategories } from "./components/ListOfCategories";
 import { GlobalStyle } from "./styles/GlobalStyles";
 import { ListOfPhotoCard } from "./components/ListOfPhotoCards";
 import { Logo } from "./components/Logo";
-export const App = () => (
-  <div>
-    <GlobalStyle />
-    <Logo />
-    <ListOfCategories />
-    <ListOfPhotoCard categoryId={1} />
-  </div>
-);
+import { PhotoCardWithQuery } from "./container/PhotoCardWithQuery";
+export const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search);
+  const detailId = urlParams.get("detail");
+  console.log(detailId);
+  return (
+    <div>
+      <GlobalStyle />
+      <Logo />
+      {detailId ? (
+        <PhotoCardWithQuery id={detailId} />
+      ) : (
+        <Fragment>
+          <ListOfCategories />
+          <ListOfPhotoCard categoryId={1} />
+        </Fragment>
+      )}
+    </div>
+  );
+};

be0e67748f569e286380b146a13136ceb5c32132
santiago lozano <santiago.lozano@qcode.co>
2021-09-30 20:54:02 -0500

    usando link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,11 @@ import React, { Fragment } from "react";
 import { ListOfCategories } from "../components/ListOfCategories";
 import { ListOfPhotoCard } from "../components/ListOfPhotoCards";
 
-export const Home = ({ id }) => {
+export const Home = ({ categoryId }) => {
   return (
     <Fragment>
       <ListOfCategories />
-      <ListOfPhotoCard categoryId={id} />
+      <ListOfPhotoCard categoryId={categoryId} />
     </Fragment>
   );
 };

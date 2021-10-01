951bd7a971fbb5356667a11473d6fdc73f796af6
santiago lozano <santiago.lozano@qcode.co>
2021-09-30 21:15:22 -0500

    Link de photo card

--- src/App.js
+++ src/App.js
@@ -2,24 +2,18 @@ import React, { Fragment } from "react";
 import { GlobalStyle } from "./styles/GlobalStyles";
 import { Home } from "./pages/Home";
 import { Logo } from "./components/Logo";
-import { PhotoCardWithQuery } from "./container/PhotoCardWithQuery";
 import { Router } from "@reach/router";
+import { Detail } from "./pages/Detail";
 export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search);
-  const detailId = urlParams.get("detail");
-  console.log(detailId);
   return (
     <div>
       <GlobalStyle />
       <Logo />
-      {detailId ? (
-        <PhotoCardWithQuery id={detailId} />
-      ) : (
-        <Router>
-          <Home path="/" />
-          <Home path="/pet/:categoryId" />
-        </Router>
-      )}
+      <Router>
+        <Home path="/" />
+        <Home path="/pet/:categoryId" />
+        <Detail path="detail/:detailId" />
+      </Router>
     </div>
   );
 };

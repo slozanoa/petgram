fd049a9833304d3d65ecdec2a465e85e4e2c09c5
DESKTOP-TCC4UPQ\user <santiago>
2021-10-03 22:28:39 -0500

    isAtuth

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import { GlobalStyle } from "./styles/GlobalStyles";
 import { Home } from "./pages/Home";
 import { Logo } from "./components/Logo";
 import { Router } from "@reach/router";
+import { NavBar } from './components/NavBar'
 import { Detail } from "./pages/Detail";
+import { Favs } from "./pages/Favs";
+import { User } from "./pages/User";
+import { NotRegisteredUser } from "./pages/NotRegisteredUser";
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 export const App = () => {
   return (
     <div>
@@ -14,6 +22,19 @@ export const App = () => {
         <Home path="/pet/:categoryId" />
         <Detail path="detail/:detailId" />
       </Router>
+      <UserLogged>
+        {({isAuth}) =>
+          isAuth ?
+            <Router>
+              <Favs path='/favs' />
+              <User path='/user' />
+            </Router>
+            : <Router>
+              <NotRegisteredUser path='/favs' />
+              <NotRegisteredUser path='/user' />
+            </Router>}
+      </UserLogged>
+      <NavBar />
     </div>
   );
 };
